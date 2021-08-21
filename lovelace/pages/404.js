c222a705bc10a7324eb72610f7516eac47f00297
Caio Teixeira <caioteixeira@MacBook-Pro-de-Caio.local>
2021-08-21 18:42:58 -0300

    First pages

---
+++ lovelace/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+export const ErrorPage = () => {
+    return (
+        <>
+            <h1>Página não encontrada, retornar para a home</h1>
+            <Link href='/home'><a>Voltar para a home</a></Link>
+        </>
+    )
+}
+
+export default ErrorPage
