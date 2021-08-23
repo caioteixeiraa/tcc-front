b35f4dc69f06591e2d98a3503ccbcc2ec6c84727
Caio Teixeira <tex.caio@gmail.com>
2021-08-22 21:20:54 -0300

    wip

--- lovelace/components/WelcomeCard/styles.js
+++ lovelace/components/WelcomeCard/styles.js
@@ -5,11 +5,16 @@ export const CardContainer = styled.div`
   border-radius: 16px;
   padding: 32px;
   text-align: center;
+  width: 300px;
   max-width: 40%;
 `
 
 export const Title = styled.h1`
+  margin: 0;
+  font-size: 32px;
 `
 
 export const Text = styled.p`
+  margin: 16px auto 24px;
+  max-width: 80%;
 `
