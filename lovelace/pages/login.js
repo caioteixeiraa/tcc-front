605fd45240845631d747853edf403878158c5630
Caio Teixeira <tex.caio@gmail.com>
2021-09-07 23:38:15 -0300

    Reset password

--- lovelace/pages/login.js
+++ lovelace/pages/login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import {
@@ -32,7 +32,11 @@ export const Login = () => {
     const handleClick = () => setShow(!show)
     const [email, setEmail] = useInput('')
     const [password, setPassword] = useInput('')
+    const { isOpen, onOpen, onClose } = useDisclosure()
     const router = useRouter()
+    const [newPassword, setNewPassword] = useInput('')
+    const [confirmNewPassword, setConfirmNewPassword] = useInput('')
+    const [emailToken, setEmailToken] = useInput('')
 
     const login = () => {
         const body = {
@@ -45,6 +49,36 @@ export const Login = () => {
             console.log("Logado!")
             window.localStorage.setItem('token', res.data.accessToken)
             router.push('dashboard')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+    const forgotPassword = () => {
+        const body = {
+            email: email
+        }
+
+        axios.patch(`${process.env.NEXT_PUBLIC_API_URL}/users/forgot`, body)
+        .then((res) => {
+            console.log("E-mail enviado")
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+    const resetPassword = () => {
+        const body = {
+            token: emailToken,
+            newPassword: newPassword,
+            confirmPassword: confirmNewPassword
+        }
+
+        axios.patch(`${process.env.NEXT_PUBLIC_API_URL}/users/reset`, body)
+        .then((res) => {
+            onClose()
         })
         .catch((err) => {
             console.log(err)
@@ -88,9 +122,68 @@ export const Login = () => {
                         <Link href='/'>
                             <Button colorScheme='telegram' variant='outline'>Voltar</Button>
                         </Link>
+                        <Button 
+                        colorScheme='telegram' 
+                        variant='text' 
+                        onClick={() => {
+                            onOpen(),
+                            forgotPassword()
+                        }}
+                        >Esqueci minha senha</Button>
                     </ButtonGroup>
                 </Stack>
             </Box>
+
+            <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Trocar senha</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        <Stack spacing={4}>
+                            <Stack spacing={2}>
+                                <Text>Foi enviado um código de 6 dígitos para o e-mail <Text as='b' d='span'>{email}</Text>, preencha-o abaixo e escolha sua nova senha.</Text>
+                                <Text fontSize='xs' as='i'>*Não esqueça de checar o spam e abas de promoções ;)</Text>
+                            </Stack>
+                            <Input 
+                                placeholder="Código de confirmação" 
+                                maxLength={6}
+                                value={emailToken}
+                                onChange={setEmailToken} 
+                            />
+                            <InputGroup>
+                            <Input
+                                placeholder="Nova senha"
+                                type={show ? 'text' : 'password'}
+                                value={newPassword}
+                                onChange={setNewPassword}
+                            />
+                            <InputRightElement>
+                                <Button p='10px' onClick={handleClick}>
+                                    {show ?
+                                        <Image src={hideIcon} width={40} height={40} /> :
+                                        <Image src={showIcon} width={40} height={40} />
+                                    }
+                                </Button>
+                            </InputRightElement>
+                        </InputGroup>
+                            <Input 
+                                placeholder="Confirme a nova senha" 
+                                type="password"
+                                value={confirmNewPassword}
+                                onChange={setConfirmNewPassword}
+                            />
+                        </Stack>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3} onClick={resetPassword}>
+                            Confirmar
+                        </Button>
+                        <Button onClick={onClose}>Cancelar</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </Flex>
     )
 }
